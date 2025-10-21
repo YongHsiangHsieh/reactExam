d83290763801980994ac65f209de97065441f5d4
Wilson <wilsonhsieh1216@gmail.com>
2025-10-21 13:20:54 +0100

    Added Now Playing page.

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,26 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Navigate, Routes } from "react-router";
-import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
 import MovieReviewPage from "./pages/movieReviewPage";
 import TrendingTodayPage from "./pages/trendingTodayPage";
-import SiteHeader from './components/siteHeader';
+import TopRatedPage from "./pages/topRatedPage";
+import UpcomingPage from "./pages/upcomingPage";
+import SiteHeader from "./components/siteHeader";
 import MoviesContextProvider from "./contexts/moviesContext";
-import AddMovieReviewPage from './pages/addMovieReviewPage'
-
-
+import AddMovieReviewPage from "./pages/addMovieReviewPage";
+import NowPlayingPage from "./pages/nowPlayingPage";
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       staleTime: 360000,
       refetchInterval: 360000,
-      refetchOnWindowFocus: false
+      refetchOnWindowFocus: false,
     },
   },
 });
@@ -35,7 +36,13 @@ const App = () => {
             <Route path="/reviews/:id" element={<MovieReviewPage />} />
             <Route path="/reviews/form" element={<AddMovieReviewPage />} />
             <Route path="/movies/:id" element={<MoviePage />} />
-            <Route path="/movies/trending/today" element={<TrendingTodayPage />} />
+            <Route
+              path="/movies/trending/today"
+              element={<TrendingTodayPage />}
+            />
+            <Route path="/movies/upcoming" element={<UpcomingPage />} />
+            <Route path="/movies/top_rated" element={<TopRatedPage />} />
+            <Route path="/movies/now_playing" element={<NowPlayingPage />} />
             <Route path="/" element={<HomePage />} />
             <Route path="*" element={<Navigate to="/" />} />
           </Routes>
@@ -46,6 +53,5 @@ const App = () => {
   );
 };
 
-
-const rootElement = createRoot(document.getElementById("root"))
+const rootElement = createRoot(document.getElementById("root"));
 rootElement.render(<App />);
